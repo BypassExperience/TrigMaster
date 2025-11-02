2a6974948ffab18114ccbf1e968b0bba60f65ffc
Mominul <mominulis7275@gmail.com>
2025-11-02 17:25:53 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+import TopicGrid from "@/components/TopicGrid";
+
+export default function HomePage() {
+  return (
+    <main className="space-y-10">
+      <Hero />
+      <TopicGrid />
+    </main>
+  );
+}
